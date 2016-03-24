08fb4c35374c42a47337831d541a0cb5310ce85e
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-03-24 07:25:09 -0500

    start of cordova app

---
+++ www/js/index.js
@@ -0,0 +1,18 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id) {
+        window.open("https://chat.leanpixel.com");
+    }
+};
+
+app.initialize();
